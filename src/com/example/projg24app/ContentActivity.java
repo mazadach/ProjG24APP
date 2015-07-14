5e0956d5ae5a39398282586ba89b019c525dc60c
Allison <allison@fuakirakens-Mac-mini.local>
2015-07-15 00:07:40 +0800

    fragment list 完成

--- src/com/example/projg24app/ContentActivity.java
+++ src/com/example/projg24app/ContentActivity.java
@@ -25,21 +25,34 @@ import android.widget.AdapterView.OnItemClickListener;
 public class ContentActivity extends Activity {
 
 	// private ListView drawerListView;
-	private Button btntest;
+	//private Button btntest;
 	private ActionBarDrawerToggle mDrawerToggle;
 	private String mTitle = "";
 	private DrawerLayout drawerLayout;
 	private ArrayList<SlideDrawerModel> arrayList;
 	private SlideDrawerAdapter leftListadapter;
+	SlideDrawerContentLeftListFragment rFragment;
 
-	public static final String MY_PROFILE = "個人檔案", THE_NEWS = "最新消息",
-			SEARCH_EXHIBITION = "探索活動", MY_FAVORITE = "我的最愛",
-			NEAR_INSPECT = "近期檢視", LOGOUT = "登出";
+	public static final String 
+					MY_PROFILE = "個人檔案", 
+					THE_NEWS = "最新消息",
+					SEARCH_EXHIBITION = "探索活動", 
+					MY_FAVORITE = "我的最愛",
+					NEAR_INSPECT = "近期檢視", 
+					LOGOUT = "登出";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_content);
+		
+		 rFragment = new SlideDrawerContentLeftListFragment();
+		 FragmentManager fragmentManager = getFragmentManager();
+		 FragmentTransaction ft = fragmentManager.beginTransaction();
+		 ft.replace(R.id.drawer_list, rFragment);
+		 ft.commit();
+		
+		
 		findView();
 		setView();
 		setAction();
@@ -48,7 +61,6 @@ public class ContentActivity extends Activity {
 	private void findView() {
 		mTitle = (String) getTitle();
 		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-		btntest = (Button) findViewById(R.id.drawer_list);
 	}
 
 	private void setView() {
@@ -65,7 +77,7 @@ public class ContentActivity extends Activity {
 
 			/** Called when a drawer is opened */
 			public void onDrawerOpened(View drawerView) {
-				getActionBar().setTitle("Select a river");
+				//getActionBar().setTitle("");
 				invalidateOptionsMenu();
 			}
 
@@ -73,47 +85,10 @@ public class ContentActivity extends Activity {
 
 		drawerLayout.setDrawerListener(mDrawerToggle);
 
-		// arrayList=new ArrayList<SlideDrawerModel>();
-		//
-		// arrayList.add(new SlideDrawerModel(R.drawable.ic_btn, MY_PROFILE));
-		// arrayList.add(new SlideDrawerModel(R.drawable.ic_btn, THE_NEWS));
-		// arrayList.add(new SlideDrawerModel(R.drawable.ic_btn,
-		// SEARCH_EXHIBITION));
-		// arrayList.add(new SlideDrawerModel(R.drawable.ic_btn, MY_FAVORITE));
-		// arrayList.add(new SlideDrawerModel(R.drawable.ic_btn, NEAR_INSPECT));
-		// arrayList.add(new SlideDrawerModel(R.drawable.ic_btn, LOGOUT));
-		//
-		// leftListadapter = new SlideDrawerAdapter(getBaseContext(),
-		// arrayList);
-
-		// drawerListView.setAdapter(leftListadapter);
-
 		getActionBar().setHomeButtonEnabled(true);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
-		// drawerListView.setOnItemClickListener(new OnItemClickListener() {
-		//
-		// @Override
-		// public void onItemClick(AdapterView<?> parent, View view,int
-		// position, long id) {
-		// // selectItem(position);
-		//
-		// }
-		// });
 	}
 
-	private void selectItem(int position) {
-		// SlideDrawerContentMainFragment rFragment = new
-		// SlideDrawerContentMainFragment();
-		// Bundle data = new Bundle();
-		// data.putInt("position", position);
-		// rFragment.setArguments(data);
-		// FragmentManager fragmentManager = getFragmentManager();
-		// FragmentTransaction ft = fragmentManager.beginTransaction();
-//		 ft.replace(R.id.content_frame, rFragment);
-		// ft.commit();
-		// drawerLayout.closeDrawer(drawerListView);
-
-	}
 
 	private void setAction() {
 
@@ -129,8 +104,6 @@ public class ContentActivity extends Activity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		if (mDrawerToggle.onOptionsItemSelected(item)) {
-			
-			Log.i("msg","open_item");
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -139,20 +112,14 @@ public class ContentActivity extends Activity {
 	/** Called whenever we call invalidateOptionsMenu() */
 	@Override
 	public boolean onPrepareOptionsMenu(Menu menu) {
-		boolean drawerOpen = drawerLayout.isDrawerOpen(btntest);
+		boolean drawerOpen = drawerLayout.isDrawerOpen(R.id.drawer_list);
 		menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
-//		SlideDrawerContentLeftListFragment fragment1 = new SlideDrawerContentLeftListFragment();  
-//         getFragmentManager().beginTransaction().replace(R.id.main_layout, fragment1).commit();  
-		
-		Log.i("msg","open");
-		
 		return super.onPrepareOptionsMenu(menu);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
-		Log.i("msg","open_menu");
 		return true;
 	}
 }
